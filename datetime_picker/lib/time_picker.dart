d8a16c9932790317344175e3b7b05faa6aa830bf
Daniel Heiß <daniel.heiss85@gmail.com>
2019-03-22 04:31:42 +0100

    Timepicker changes to minutes automatically! Timepicker in German!

--- datetime_picker/lib/time_picker.dart
+++ datetime_picker/lib/time_picker.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -11,10 +13,17 @@ class TimePicker extends StatefulWidget {
 class _TimePickerState extends State<TimePicker> {
   TimeOfDay _time = new TimeOfDay.now();
 
-  Future<Null> _selectTime(BuildContext context) async {
+  Future<void> _selectTime(BuildContext context) async {
     final TimeOfDay _picked = await showTimePicker(
-        context: context,
-        initialTime: _time,
+      context: context,
+      initialTime: _time,
+      builder: (BuildContext context, Widget child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+          child: child,
+        );
+      },
+      locale: const Locale('de'),
     );
 
     if ((_picked != null) && (_picked != _time)) {
@@ -27,9 +36,11 @@ class _TimePickerState extends State<TimePicker> {
   @override
   Widget build(BuildContext context) {
     return new GestureDetector(
-      onTap: () {_selectTime(context);},
-      child: new Text(new DateFormat('HH:mm').format(new DateTime(2000, 1, 1, _time.hour, _time.minute))),
-
+      onTap: () {
+        _selectTime(context);
+      },
+      child: new Text(new DateFormat('HH:mm')
+          .format(new DateTime(2000, 1, 1, _time.hour, _time.minute))),
     );
   }
 }
