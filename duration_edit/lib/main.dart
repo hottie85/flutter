e6d80b4a2eb88ddf2167776c886f9acafd7b9ed2
Daniel Heiß <daniel.heiss85@gmail.com>
2019-05-16 01:30:14 +0200

    App in German

--- duration_edit/lib/main.dart
+++ duration_edit/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:duration_edit/duration_buttons.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,6 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: GlobalMaterialLocalizations.delegates,
+      supportedLocales: [
+        const Locale('de', 'DE'),
+        // ...
+      ],
       title: 'Dauer Edit',
       home: MyHomePage(title: 'Dauer Edit'),
       theme: ThemeData(
