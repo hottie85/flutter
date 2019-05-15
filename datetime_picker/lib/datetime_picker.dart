f88ad0df4350d1e52a9a20208a5b1078ba9aa9c5
Daniel Heiß <daniel.heiss85@gmail.com>
2019-05-16 01:25:02 +0200

    Updated DateTimePicker

--- datetime_picker/lib/datetime_picker.dart
+++ datetime_picker/lib/datetime_picker.dart
@@ -20,48 +20,40 @@ class DateTimePicker extends StatefulWidget {
 }
 
 class _DateTimePickerState extends State<DateTimePicker> {
-  DateTime _dateTime;
-
-  @override
-  void initState() {
-    _dateTime = widget.initialDate;
-    print(widget.initialDate.toString());
-    _dateTime = DateTime(_dateTime.year, _dateTime.month, _dateTime.day,
-        _dateTime.hour, _dateTime.minute);
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      child: Text(DateFormat('dd.MM.yyyy HH:mm').format(_dateTime)),
+      child: Text(DateFormat('dd.MM.yyyy HH:mm').format(widget.initialDate)),
       onLongPress: () async {
         DateTime _selectedDate = await showDatePicker(
           context: context,
           firstDate: widget.firstDate,
-          initialDate: _dateTime,
+          initialDate: widget.initialDate,
           lastDate: widget.lastDate,
           initialDatePickerMode: DatePickerMode.day,
         );
         if (_selectedDate != null) {
-          setState(() {
-            _dateTime = DateTime(_selectedDate.year, _selectedDate.month,
-                _selectedDate.day, _dateTime.hour, _dateTime.minute);
-          });
-          widget.onDateTimeChanged(_dateTime);
+          widget.onDateTimeChanged(DateTime(
+              _selectedDate.year,
+              _selectedDate.month,
+              _selectedDate.day,
+              widget.initialDate.hour,
+              widget.initialDate.minute));
         }
       },
       onTap: () async {
         TimeOfDay _selectedTime = await showTimePicker(
             context: context,
-            initialTime:
-                TimeOfDay(hour: _dateTime.hour, minute: _dateTime.minute));
+            initialTime: TimeOfDay(
+                hour: widget.initialDate.hour,
+                minute: widget.initialDate.minute));
         if (_selectedTime != null) {
-          setState(() {
-            _dateTime = DateTime(_dateTime.year, _dateTime.month, _dateTime.day,
-                _selectedTime.hour, _selectedTime.minute);
-          });
-          widget.onDateTimeChanged(_dateTime);
+          widget.onDateTimeChanged(DateTime(
+              widget.initialDate.year,
+              widget.initialDate.month,
+              widget.initialDate.day,
+              _selectedTime.hour,
+              _selectedTime.minute));
         }
       },
     );
