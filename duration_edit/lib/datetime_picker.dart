66b5e556b626c354f41cdb78b488ed41c963aa1b
Daniel Heiß <daniel.heiss85@gmail.com>
2019-05-17 20:47:35 +0200

    Use the new DateTimePicker. It shows DatePicker when press on Date and TimePicker when press on time.

--- duration_edit/lib/datetime_picker.dart
+++ duration_edit/lib/datetime_picker.dart
@@ -20,42 +20,55 @@ class DateTimePicker extends StatefulWidget {
 }
 
 class _DateTimePickerState extends State<DateTimePicker> {
+  DateTime _dateTime;
+
+  @override
+  void initState() {
+    _dateTime = widget.initialDate;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      child: Text(DateFormat('dd.MM.yyyy HH:mm').format(widget.initialDate)),
-      onLongPress: () async {
-        DateTime _selectedDate = await showDatePicker(
-          context: context,
-          firstDate: widget.firstDate,
-          initialDate: widget.initialDate,
-          lastDate: widget.lastDate,
-          initialDatePickerMode: DatePickerMode.day,
-        );
-        if (_selectedDate != null) {
-          widget.onDateTimeChanged(DateTime(
-              _selectedDate.year,
-              _selectedDate.month,
-              _selectedDate.day,
-              widget.initialDate.hour,
-              widget.initialDate.minute));
-        }
-      },
-      onTap: () async {
-        TimeOfDay _selectedTime = await showTimePicker(
-            context: context,
-            initialTime: TimeOfDay(
-                hour: widget.initialDate.hour,
-                minute: widget.initialDate.minute));
-        if (_selectedTime != null) {
-          widget.onDateTimeChanged(DateTime(
-              widget.initialDate.year,
-              widget.initialDate.month,
-              widget.initialDate.day,
-              _selectedTime.hour,
-              _selectedTime.minute));
-        }
-      },
+    return Row(
+      children: <Widget>[
+        GestureDetector(
+          child: Text(DateFormat('dd.MM.yyyy').format(_dateTime)),
+          onTap: () async {
+            DateTime _selectedDate = await showDatePicker(
+              context: context,
+              firstDate: widget.firstDate,
+              initialDate: _dateTime,
+              lastDate: widget.lastDate,
+              initialDatePickerMode: DatePickerMode.day,
+            );
+            if (_selectedDate != null) {
+              setState(() {
+                _dateTime = DateTime(_selectedDate.year, _selectedDate.month,
+                    _selectedDate.day, _dateTime.hour, _dateTime.minute);
+              });
+              widget.onDateTimeChanged(_dateTime);
+            }
+          },
+        ),
+        Text(' '),
+        GestureDetector(
+          child: Text(DateFormat('HH:mm').format(_dateTime)),
+          onTap: () async {
+            TimeOfDay _selectedTime = await showTimePicker(
+                context: context,
+                initialTime:
+                    TimeOfDay(hour: _dateTime.hour, minute: _dateTime.minute));
+            if (_selectedTime != null) {
+              setState(() {
+                _dateTime = DateTime(_dateTime.year, _dateTime.month,
+                    _dateTime.day, _selectedTime.hour, _selectedTime.minute);
+              });
+              widget.onDateTimeChanged(_dateTime);
+            }
+          },
+        )
+      ],
     );
   }
 }
